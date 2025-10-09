16ba1a8abad41c0062bebdf3cad01eedaa565dba
Gurveer Singh <sgurveer97@gmail.com>
2025-10-09 20:03:12 +0530

    Day 16: Variables, data types, operators

---
+++ Day-16/README.md
@@ -0,0 +1,17 @@
+# 🗓️ Day 16 — JavaScript Basics
+
+## 📘 Topics
+- Variables  
+- Data Types  
+- Operators  
+
+---
+
+## 🟨 JavaScript Introduction
+JavaScript adds interactivity and logic to web pages.  
+You can write it directly inside the HTML file or in a separate `.js` file.
+
+```html
+<script>
+  console.log("Hello, JavaScript!");
+</script>
---
+++ Day-16/script.js
@@ -0,0 +1,39 @@
+//DATA-TYPES
+let name = "Guri";
+let age = 21;
+let fruits = ["apple", "banana", "cherry"];
+let person = { name: "Guri", country: "India" };
+
+console.log(typeof name); // string
+console.log(typeof age);  // number
+console.log(typeof fruits); // object
+console.log(typeof person); // object
+
+
+//OPERATORS:
+let x = 10, y = 5;
+
+console.log(x + y);  // 15
+console.log(x - y);  // 5
+console.log(x * y);  // 50
+console.log(x / y);  // 2
+console.log(x % y);  // 0
+console.log(x > y);  // true
+console.log(x === y); // false
+
+
+//  VARIABLE CREATION
+let userName = "GURVEER";
+const birthYear = 2003;
+let isStudent = true;
+
+console.log(userName, birthYear, isStudent);
+
+//SIMPLE CALCULATOR
+let a = 8, b = 3;
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Remainder:", a % b);
