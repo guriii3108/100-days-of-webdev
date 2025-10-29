9e5fa840e9e2b450c0ca60be2260fe2fc8c5e188
Gurveer Singh <sgurveer97@gmail.com>
2025-10-29 20:00:57 +0530

    DAY-35-36 GSAP

---
+++ Day-35-36/GSAP/1.INTRO/script.js
@@ -0,0 +1,80 @@
+// gsap.from("#box1",{
+//       x:1200,
+//       duration:2,
+//       delay:1,
+//       rotate:360,
+//       backgroundColor:"blue",
+//       borderRadius:"50%",
+//       scale:0.5
+// })
+
+// gsap.from("#box2",{
+//       x:500,
+//       y:200,
+//       duration:2,
+//       delay:1
+// })
+
+
+// gsap.from("h1",{
+//       opacity:0,
+//       duration:3,
+//       y:20,
+//       stagger:-1
+// })
+
+// // -----------------------------
+// // REPEAT AND YOYO(BACK AND FORTH MOTION)
+// gsap.to("#box1",{
+//       x:1200,
+//       duration:1,
+//       delay:1,
+//       rotate:360,
+//       repeat:2,
+//       yoyo:true
+// })
+
+
+// // ==============================
+// // GSAP TIMELINE
+// const tl=gsap.timeline() //now this is timeline
+// tl.to("#box1",{
+//       x:1200,
+//       duration:1.5,
+//       delay:1,
+//       rotate:360,
+// })
+// tl.to("#box2",{
+//       x:1200,
+//       duration:1.5,
+//       rotate:360,
+// })
+// tl.to("#box3",{
+//       x:1200,
+//       duration:1.5,
+//       rotate:360,
+// })
+
+// // ==============================
+// let tl=gsap.timeline()
+// tl.from("h2",{
+//       y:-30,
+//       opacity:0,
+//       duration:1,
+//       delay:0.5
+// })
+// tl.from("h4",{
+//       y:-30,
+//       stagger:0.5,
+//       opacity:0,
+//       duration:1,
+// })
+// tl.from("h1",{
+//       y:20,
+//       opacity:0,
+//       duration:0.5,
+//       scale:0.2
+// })
+
+// // ==============================
+
